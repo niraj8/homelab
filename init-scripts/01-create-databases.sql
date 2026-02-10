25990ec1ee618895cd43cbf6140f15a62089ac7c
Niraj Palecha <8666468+niraj8@users.noreply.github.com>
2026-02-10 17:16:16 +0530

    Closes #1: add shiori bookmark manager

--- init-scripts/01-create-databases.sql
+++ init-scripts/01-create-databases.sql
@@ -1,4 +1,6 @@
 -- Create additional databases
 CREATE DATABASE upsite_sqlc_vet;
+CREATE DATABASE pune_team_dev;
+CREATE DATABASE shiori;
 -- You can add more databases here as needed
 -- CREATE DATABASE another_database_name;
